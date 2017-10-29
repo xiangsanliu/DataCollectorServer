ccc9708aa023a23d940431a775d73c55437e5fc4
xiang <xiangjianjian@hotmail.com>
2017-10-29 12:32:07 +0800

    Initial commit

---
+++ src/main/java/com/wangxiang/datacollectorserver/DataCollectorServerApplication.java
@@ -0,0 +1,14 @@
+package com.wangxiang.datacollectorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.wangxiang.datacollectorserver")
+public class DataCollectorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectorServerApplication.class, args);
+	}
+}
