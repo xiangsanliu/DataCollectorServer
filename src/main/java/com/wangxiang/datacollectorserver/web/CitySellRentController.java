76a8f79ed0d7a0de6541f07d1db2319dcb0cd26c
xiang <xiangjianjian@hotmail.com>
2017-12-05 20:35:34 +0800

    服务器IP地址改了

--- src/main/java/com/wangxiang/datacollectorserver/web/CitySellRentController.java
+++ src/main/java/com/wangxiang/datacollectorserver/web/CitySellRentController.java
@@ -4,7 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.wangxiang.datacollectorserver.domain.dao.*;
 import com.wangxiang.datacollectorserver.domain.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,32 +41,32 @@ public class CitySellRentController {
         this.shopRentRepository = shopRentRepository;
     }
 
-    @PostMapping("/get/citysellrents")
+    @GetMapping("/get/citysellrents")
     public List<CitySellRent> getCitySellRents(Long userId) {
         return citySellRentRepository.findCitySellRentsByUserId(userId);
     }
 
-    @PostMapping("/get/citysellrent")
+    @GetMapping("/get/citysellrent")
     public CitySellRent getCitySellRent(Long id) {
         return citySellRentRepository.findCitySellRentById(id);
     }
 
-    @PostMapping("/get/commercial")
+    @GetMapping("/get/commercial")
     public CommercialHouseTradeModel getCommercialHouseTrade(Long id) {
         return commercialHouseTradeRepository.findCommercialHouseTradeModelById(id);
     }
 
-    @PostMapping("/get/houserent")
+    @GetMapping("/get/houserent")
     public HouseRentModel getHouseRent(Long id) {
         return houseRentRepository.findHouseRentModelById(id);
     }
 
-    @PostMapping("/get/housetrade")
+    @GetMapping("/get/housetrade")
     public HouseTradeModel getHouseTrade(Long id) {
         return houseTradeRepository.findHouseTradeModelById(id);
     }
 
-    @PostMapping("/get/shoprent")
+    @GetMapping("/get/shoprent")
     public ShopRentModel getShopRent(Long id) {
         return shopRentRepository.findShopRentModelById(id);
     }
@@ -78,33 +79,33 @@ public class CitySellRentController {
 
 
 
-    @PostMapping("/save/city")
+    @GetMapping("/save/city")
     public void saveCitySellRent(String content) {
         System.out.println(content);
         citySellRentRepository.save(JSON.parseObject(content, CitySellRent.class));
     }
 
-    @PostMapping("/save/commercial")
+    @GetMapping("/save/commercial")
     public void saveCommercialHouseTrade(String content) {
         commercialHouseTradeRepository.save(JSON.parseObject(content, CommercialHouseTradeModel.class));
     }
 
-    @PostMapping("/save/houserent")
+    @GetMapping("/save/houserent")
     public void saveHouseRent(String content) {
         houseRentRepository.save(JSON.parseObject(content, HouseRentModel.class));
     }
 
-    @PostMapping("/save/housetrade")
+    @GetMapping("/save/housetrade")
     public void saveHouseTrade(String content) {
         houseTradeRepository.save(JSON.parseObject(content, HouseTradeModel.class));
     }
 
-    @PostMapping("/save/shoprent")
+    @GetMapping("/save/shoprent")
     public void saveShopRent(String content) {
         shopRentRepository.save(JSON.parseObject(content, ShopRentModel.class));
     }
 
-    @PostMapping("/delete/city")
+    @GetMapping("/delete/city")
     public void deleteCitySellRent(Long id) {
         citySellRentRepository.delete(id);
         commercialHouseTradeRepository.delete(id);
@@ -113,13 +114,13 @@ public class CitySellRentController {
         houseTradeRepository.delete(id);
     }
 
-    @PostMapping("/test")
+    @GetMapping("/test")
     public void test(String xiang) {
         System.out.println(xiang);
         System.out.println("xiangjianjian");
     }
 
-    @PostMapping("/querytest")
+    @GetMapping("/querytest")
     public CitySellRent testquery(String stringId) {
         System.out.println(stringId);
         Long id = Long.valueOf(stringId);
