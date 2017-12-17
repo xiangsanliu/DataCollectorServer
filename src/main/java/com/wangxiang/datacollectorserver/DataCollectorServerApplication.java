1c2ba0101440c6ad12995f5b9fa24e175973b571
xiang <xiangjianjian@hotmail.com>
2017-12-17 16:58:22 +0800

    用上了多环境配置

--- src/main/java/com/wangxiang/datacollectorserver/DataCollectorServerApplication.java
+++ src/main/java/com/wangxiang/datacollectorserver/DataCollectorServerApplication.java
@@ -8,10 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.wangxiang.datacollectorserver")
 public class DataCollectorServerApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DataCollectorServerApplication.class, args);
 	}
-
 
 }
